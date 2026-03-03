135caf309458b334d107b9a4f482fd7c5ecb3ad7
mazharul-15 <mazharulislamhstu15@gmail.com>
2026-03-03 23:32:19 +0600

    adding List declaration method

--- src/main/kotlin/5.2.List.kt
+++ src/main/kotlin/5.2.List.kt
@@ -6,7 +6,9 @@ fun main(args: Array<String>) {
 }
 
 fun immutableList() {
-    val ls = listOf("Hajee", "Mohammad", "Danesh", "Science", "and", "Technology", "University")t
+    val ls = listOf("Hajee", "Mohammad", "Danesh", "Science", "and", "Technology", "University")
+    val lsNew = listOf<String>("I", "Love", "Bangladesh", "Bro")
+    val lsNew2: List<String> = listOf("Android", "Engineer", "Remote","Home Office")
 }
 
 fun mutableList() {
