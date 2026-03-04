ba7dd364ddc6bb9679e66062c80dc65411b8bb47
mazharul-15 <mazharulislamhstu15@gmail.com>
2026-03-04 17:37:59 +0600

    adding mutable list

--- src/main/kotlin/5.2.List.kt
+++ src/main/kotlin/5.2.List.kt
@@ -8,9 +8,11 @@ fun main(args: Array<String>) {
 fun immutableList() {
     val ls = listOf("Hajee", "Mohammad", "Danesh", "Science", "and", "Technology", "University")
     val lsNew = listOf<String>("I", "Love", "Bangladesh", "Bro")
-    val lsNew2: List<String> = listOf("Android", "Engineer", "Remote","Home Office")
+    val lsNew2: List<String> = listOf("Android", "Engineer", "Remote","Work from Home-Office")
 }
 
 fun mutableList() {
-
+    val ls = mutableListOf("A", "B", "c", "D")
+    val lsNew = mutableListOf<String>("Hajee", "Mohammad", "Danesh", "Science", "and", "Technology", "University")
+    val lsNew2: MutableList<String> = mutableListOf("I", "Love", "Bangladesh")
 }
