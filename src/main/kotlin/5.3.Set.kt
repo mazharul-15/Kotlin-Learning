64eefa7184467f4268647e86f198e9084ef8f3ef
mazharul-15 <mazharulislamhstu15@gmail.com>
2026-03-15 16:48:49 +0600

    adding set

---
+++ src/main/kotlin/5.3.Set.kt
@@ -0,0 +1,29 @@
+// Set: Set is a collection that does not allow duplicates
+
+fun main() {
+    setDeclaration()
+}
+
+fun setDeclaration() {
+    // immutable set
+    val set1 = setOf("A", "A", "B", "B", "C")
+    val set2 = setOf<String>("A", "A", "B", "B", "C")
+    val set3: Set<String> = setOf("I", "Love", "I", "Love", "BD")
+    val set4 = setOf<String>() // empty set
+
+    println("Set1: $set1")
+    println("Set2: $set2")
+    println("Set3: $set3")
+    println("Set4: $set4")
+
+    // mutable set
+    val setM1 = mutableSetOf("A", "X", "B", "A", "X")
+    val setM2 = mutableSetOf<String>("I", "I", "Love", "BD")
+    val setM3: MutableSet<String> = mutableSetOf("B", "C", "B", "C", "D", "E")
+    val setM4 = mutableSetOf<String>() // empty mutable set
+
+    println("SetM1: $setM1")
+    println("SetM2: $setM2")
+    println("SetM3: $setM3")
+    println("SetM4: $setM4")
+}
