c6203358235ff4ef4277cbbc6234c4782db095aa
mazharul-15 <mazharulislamhstu15@gmail.com>
2026-03-23 18:24:45 +0600

    feat: null safety function

--- src/main/kotlin/8.1.Null-Safety.kt
+++ src/main/kotlin/8.1.Null-Safety.kt
@@ -5,5 +5,19 @@ fun main() {
 }
 
 fun nullSafety() {
+    val strNull: String? = null
+    var strNull2: String? = null
+
+    println("Length: ${strNull?.length}")
+    println("Length: ${strNull2?.length}")
+
+
+    // nullable list
+    val list2: MutableList<String> = mutableListOf()
+    val list: MutableList<String>? = mutableListOf()
+    println(list)
+    println(list?.size)
+    println("Size of List:${list2.size}")
+
 
 }
