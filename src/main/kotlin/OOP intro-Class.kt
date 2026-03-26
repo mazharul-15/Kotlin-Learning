462e03d01e37c4a0685c09fa5ec0b3846ed1d41d
mazharul-15 <mazharulislamhstu15@gmail.com>
2026-03-26 23:14:23 +0600

    feat: car class added into 6.1.OOP intro-class.kt file

---
+++ src/main/kotlin/OOP intro-Class.kt
@@ -0,0 +1,20 @@
+/*
+    OOP = A way of writing code where everything is organised in one place ( data + behavior) as
+          a real world object
+*/
+
+class Car(val carName: String, val carBrandName: String) {
+    fun start() {
+        println("$carName is started")
+    }
+
+    fun stop() {
+        println("$carName is stopped")
+    }
+}
+
+fun main() {
+    val car1 = Car("Fielder", "Toyota")
+    car1.start()
+    car1.stop()
+}
