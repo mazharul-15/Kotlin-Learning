fe82fc68adefe26da106e21afa7373f7573051c8
mazharul-15 <mazharulislamhstu15@gmail.com>
2026-04-01 18:37:32 +0600

    feat: constructor type added

--- src/main/kotlin/6.3.Constructor.kt
+++ src/main/kotlin/6.3.Constructor.kt
@@ -16,7 +16,23 @@ class Student2(var name: String, var Id: Int, var age: Int) {
 }
 
 // init block
+class Student3(var name: String, var id: Int) {
 
+    init {
+        println("Object is created with the name of : $name and Id: $id")
+    }
+    fun getInfo() {
+        println("Name: $name and Id: $id")
+    }
+}
+
+
+// default parameter values in primary constructor
+class Student4(val name: String = "Khairul Basar", val age: Int = 35) {
+    fun getinfo() {
+        println("Name: $name and Age: $age")
+    }
+}
 
 
 // Secondary Constructor
@@ -25,6 +41,11 @@ fun main() {
     val p1 = Person2(4, 5)
     p1.getInfo()
 
-    val student1 = Student2("Zahir", 1203044, 32)
-    student1.getInfo()
+    val student2 = Student2("Zahir", 1203044, 32)
+    student2.getInfo()
+
+    val student3 = Student3("Ammir", 101)
+
+    val student4 = Student4()
+    student4.getinfo()
 }
