2f1837a25a374d14ab94d9f0ebd42cacc6259e90
mazharul-15 <mazharulislamhstu15@gmail.com>
2026-04-05 22:09:06 +0600

    feat: added visibility modifier

---
+++ src/main/kotlin/6.5.Kotlin Visibility Modifiers.kt
@@ -0,0 +1,17 @@
+/*
+    Visibility Modifiers:
+        Public
+        Private
+        Protected
+        Internal
+*/
+
+class Person{
+    private var name: String = ""
+    protected var age: Int = 0
+}
+
+fun main() {
+
+
+}
