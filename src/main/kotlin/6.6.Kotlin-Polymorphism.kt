108384d1553fc674e32bca4cb39d0a85e832bdad
mazharul-15 <mazharulislamhstu15@gmail.com>
2026-04-11 21:24:56 +0600

    feat: polymorphism: overrding

--- src/main/kotlin/6.6.Kotlin-Polymorphism.kt
+++ src/main/kotlin/6.6.Kotlin-Polymorphism.kt
@@ -1,5 +1,24 @@
-// Kotlin Inheritance
+/*
+    Kotlin Polymorphism
+        1. Method Overriding
+        2. Method Overloading
 
+        same function but behaves different
+
+        🧠 Real-Life Example
+
+            Think of a person:
+
+            As a teacher → teaches
+            As a father → cares
+            As a friend → jokes
+
+            👉 Same person, different behavior
+            👉 This is polymorphism
+*/
+
+
+// #Overriding
 open class Animal {
     open fun sound() {
         println("This is sound of animals")
@@ -9,22 +28,35 @@ open class Animal {
 class Dog: Animal() {
     override fun sound() {
         //super.sound()
-        println("This the sound of Dog")
+        println("This the sound of Dog: Ghewowwww!!!")
     }
 }
 
 class Cat: Animal() {
     override fun sound() {
         //super.sound()
-        println("This is the sound of Cat")
+        println("This is the sound of Cat: Meoww!!!!!")
     }
 }
 
+class Cow: Animal() {
+    override fun sound() {
+        println("This is the sound of Cow: Hambaaaa!!!!!")
+    }
+}
+
+
 fun main() {
+    // Overriding
+    val animal = Animal()
+    animal.sound()
     val dog = Dog()
     dog.sound()
     val cat = Cat()
     cat.sound()
-    val animal = Animal()
-    animal.sound()
+    val cow = Cow()
+    cow.sound()
+
+    // Overloading
+
 }
