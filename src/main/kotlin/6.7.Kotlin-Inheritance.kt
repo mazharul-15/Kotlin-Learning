1cd61e7b2ad9aff49fbd7bc01f5104af21fde3a7
mazharul-15 <mazharulislamhstu15@gmail.com>
2026-04-14 22:39:53 +0600

    feat: adding inheritance

--- src/main/kotlin/6.7.Kotlin-Inheritance.kt
+++ src/main/kotlin/6.7.Kotlin-Inheritance.kt
@@ -2,7 +2,7 @@
 
     Inheritance:
  */
-
+// Inheritance basics
 open class Base {
     open fun show() {
         println("Hello I am Base class!!!")
@@ -15,6 +15,41 @@ class Child: Base(){
     }
 }
 
+// Inheritance properties and methods
+open class User {
+    var username: String = ""
+    var userpassword: String = ""
+    fun show(name: String) {
+        println("$name logged in succesfully")
+    }
+}
+
+class Admin: User() {
+    fun loggedIN(name: String, password: String) {
+        if(super.username == name && super.userpassword == password) {
+            super.show(name)
+        }
+    }
+}
+
+class NormalUser: User() {
+    fun loggedIN(name: String, password: String) {
+        if(super.username == name && super.userpassword == password) {
+            super.show(name)
+        }
+    }
+}
+
+// kotlin inheritance primary constructor
+
+
 fun main() {
+    // inheritance basics
     val child = Child()
+
+    // inheritance properties and methods
+    val normalUser = NormalUser()
+    normalUser.username = "mazhar"
+    normalUser.userpassword = "123Maz"
+    normalUser.loggedIN("mazhar", "123Maz")
 }
