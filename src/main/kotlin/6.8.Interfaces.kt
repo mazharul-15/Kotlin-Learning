48af955e4bf30825be86298b4875bf5c51ee8985
mazharul-15 <mazharulislamhstu15@gmail.com>
2026-04-18 15:51:58 +0600

    feat: interface added more class

--- src/main/kotlin/6.8.Interfaces.kt
+++ src/main/kotlin/6.8.Interfaces.kt
@@ -1,5 +1,6 @@
 /*
     Interface: a contract (rules) that a class must follow
+    when every class must have common properties and methods then interface use
  */
 
 interface ClickListener {
@@ -12,7 +13,39 @@ class Button: ClickListener {
     }
 }
 
+interface Animal2 {
+    fun sound()
+}
+
+class Dog2: Animal2 {
+    override fun sound() {
+        println("The Dog sound: Gheu Gheu!!!")
+    }
+}
+
+class Cat2: Animal2 {
+    override fun sound() {
+        println("The Cat sound: Mewo Mewo!!!")
+    }
+}
+
+class Cow2: Animal2 {
+    override fun sound() {
+        println("The Cow sound: Hamba Hamba!!!")
+    }
+}
+
+
 fun main() {
     val button = Button()
     button.onClick()
+
+    val dog = Dog2()
+    dog.sound()
+
+    val cat = Cat2()
+    cat.sound()
+
+    val cow = Cow2()
+    cow.sound()
 }
