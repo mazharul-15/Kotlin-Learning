7b3dd687b1679159b017a40b81019136ffb573ea
mazharul-15 <mazharulislamhstu15@gmail.com>
2026-05-20 21:54:43 +0600

    feat: adding function

--- src/main/kotlin/0.1.Launch Pad-1.kt
+++ src/main/kotlin/0.1.Launch Pad-1.kt
@@ -16,7 +16,9 @@ fun main(args: Array<String>) {
 }
 
 fun DataTypeAndVariable() {
-
+    val number12: Int = 12
+    var number13: Int = readLine()!!.toInt()
+    var number14 = readLine()?.toInt()
 
 }
 
@@ -38,4 +40,21 @@ fun arrayAll() {
 
 fun stringAll() {
 
+}
+
+// functions
+fun addTwo(num1: Int, num2: Int): Int = num1 + num2
+
+fun addTwo2(num1: Int, num2: Int): Int {
+    return num1 + num2
+}
+
+fun namedArgument(userName: String, password: String, email: String, username: String, Password: String, Email: String): Boolean {
+    return (userName==username && password==Password && email==Email)
+}
+
+fun checkAdmin(userName: String, password: String, email: String): Boolean {
+    return (userName == "Admin" &&
+            password == "Pi12aDmIn" &&
+            email == "admin@gmail.com")
 }
